10e80ca420ee17ee64576accbe1cf18d5494db6c
Mynul Islam <mynul.sakil@gmail.com>
2026-01-21 21:45:48 +0600

    close the server work

--- generated/prisma/wasm.js
+++ generated/prisma/wasm.js
@@ -202,6 +202,7 @@ const config = {
     "db"
   ],
   "activeProvider": "sqlite",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -7,11 +7,26 @@ import {
 } from "~/server/api/trpc";
 
 export const todoRouter = createTRPCRouter({
-  all: protectedProcedure.query(() => {
+  all: protectedProcedure.query(async ({ctx}) => {
+  //   const todos=await ctx.db.todo.findMany({
+  //     where:{userId:ctx.session.user.id},
+  //   });
+  //  console.log('todos from prisma',todos.map(({id,text,done})=>({id,text,done})));
     return [
       { id: "fake1", text: "Learn T3 Stack", done: false },
       { id: "fake2", text: "Build a cool app", done: true },
 
     ];
   }),
+  create: protectedProcedure
+    .input(z.object({ text: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      const todo = await ctx.db.todo.create({
+        data: {
+          text: input.text,
+          userId: ctx.session.user.id,
+        },
+      });
+      return { id: todo.id, text: todo.text, done: todo.done };
+    }),
 });
